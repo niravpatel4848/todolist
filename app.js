d4f7ce08ab46e6e3e23eacbb056b5c4f6ffd3e1c
Neerav Patel <niravpatel4848@gmail.com>
2021-09-13 02:53:49 -0600

    apply css

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let items = ['Buy food.', 'Cook food.'];
+
+
 app.get("/", function (req, res) {
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error!");
+    
+    let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
     }
 
-    res.render("list", { dayOfWeek: day });
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { dayOfWeek: day, newListItems: items });
+});
+
+app.post("/", function(req,res){
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
